58d9b971b37c206201ff03221a3479f7a78bdc33
gassun <halohs50063@gmail.com>
2017-12-04 18:54:55 +0900

    ルーティング書き換え

--- main.go
+++ main.go
@@ -22,6 +22,45 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
+	r.GET("/campaign", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "campaign.html", nil)
+	})
+	r.GET("/cinemaDay", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "cinemaDay.html", nil)
+	})
+	r.GET("/comingSoon", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "comingSoon.html", nil)
+	})
+	r.GET("/coupleDay", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "coupleDay.html", nil)
+	})
+	r.GET("/customerEdit", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "customerEdit.html", nil)
+	})
+	r.GET("/ladysDay", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "ladysDay.html", nil)
+	})
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+	r.GET("/mypage", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "mypage.html", nil)
+	})
+	r.GET("/registerComp", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "registerComp.html", nil)
+	})
+	r.GET("/registerConfirm", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "registerConfirm.html", nil)
+	})
+	r.GET("/registerCustomer", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "registerCustomer.html", nil)
+	})
+	r.GET("/seniorDay", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "seniorDay.html", nil)
+	})
+	r.GET("/watchFirm", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "watchFirm.html", nil)
+	})
 
 	// おーえっち
 	r.GET("/seat", func(c *gin.Context) {
@@ -36,7 +75,6 @@ func main() {
 	r.GET("/payment", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "payment.html", nil)
 	})
-<<<<<<< HEAD
 	r.GET("/campaign", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "campaign.html", nil)
 	})
@@ -79,8 +117,6 @@ func main() {
 	r.GET("/watchFirm", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "watchFirm.html", nil)
 	})
-=======
->>>>>>> 05327b6cf3c835e4b23d01c41b09c8cb1215b11a
 
 	api := r.Group("/api")
 	{
