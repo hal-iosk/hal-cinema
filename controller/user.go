ded3331c4cb4f3b6e26537b97d3a43ce60193ed0
makki0205 <llxo2_5oxll@icloud.com>
2017-12-06 16:04:06 +0900

    登録API完成

--- controller/user.go
+++ controller/user.go
@@ -1,18 +1,82 @@
 package controller
 
 import (
-	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hal-iosk/hal-cinema/model"
+	"github.com/hal-iosk/hal-cinema/service"
 )
 
-type User struct {
-	name string
+var DayFormat = "2006/01/02"
+var TimeFormat = "2006-01-02 15:04:05"
+
+type CustomerReq struct {
+	Email            string    `gorm:"not null;unique" json:"email"`
+	Password         string    `gorm:"not null" json:"password"`
+	FirstName        string    `gorm:"not null" json:"first_name"`
+	LastName         string    `gorm:"not null" json:"last_name"`
+	FirstNameRead    string    `gorm:"not null" json:"first_name_read"`
+	LastNameRead     string    `gorm:"not null" json:"last_name_read"`
+	Phone            string    `gorm:"not null" json:"phone"`
+	Address          string    `gorm:"not null" json:"address"`
+	Birthdate        time.Time `json:"birthday"`
+	Magazine         bool      `gorm:"not null" json:"magazine"`
+	CreditCardLimit  string    `gorm:"not null" json:"credit_card_limit"`
+	CreditCardNumber string    `gorm:"not null" json:"credit_card_number"`
+	SecurityCode     string    `gorm:"not null" json:"security_code"`
 }
 
 func CreateUser(c *gin.Context) {
-	// _ := c.PostForm("name")
-	// _ := c.PostForm("password")
+	req, err := CustomerReq{}.create(c)
+	if err != nil {
+		return
+	}
+	if service.Customer.Exists(req.Email) {
+		Batequest("メアド使用済み", c)
+		return
+	}
+	service.Customer.Create(model.Customer{
+		Email:            req.Email,
+		Password:         req.Password,
+		FirstName:        req.FirstName,
+		LastName:         req.LastName,
+		FirstNameRead:    req.FirstNameRead,
+		LastNameRead:     req.LastNameRead,
+		Phone:            req.Phone,
+		Address:          req.Address,
+		Birthdate:        &req.Birthdate,
+		Magazine:         req.Magazine,
+		PointCount:       0,
+		CreditCardLimit:  req.CreditCardLimit,
+		CreditCardNumber: req.CreditCardNumber,
+		SecurityCode:     req.SecurityCode,
+	})
+	c.JSON(200, "ok!!")
+}
 
-	c.JSON(http.StatusCreated, nil)
+func (self CustomerReq) create(c *gin.Context) (CustomerReq, error) {
+	var err error
+	self.Email = c.Query("email")
+	self.Password = c.Query("password")
+	self.FirstName = c.Query("first_name")
+	self.LastName = c.Query("last_name")
+	self.FirstNameRead = c.Query("first_name_read")
+	self.LastNameRead = c.Query("last_name_read")
+	self.Phone = c.Query("phone")
+	self.Address = c.Query("address")
+	self.Birthdate, err = time.Parse(DayFormat, c.Query("birthday"))
+	if err != nil {
+		Batequest("日付けのフォーマットがおかしいよん", c)
+		return self, err
+	}
+	self.Magazine = toBool(c.Query("magazine"))
+	self.CreditCardLimit = c.Query("credit_card_limit")
+	self.CreditCardNumber = c.Query("credit_card_number")
+	self.SecurityCode = c.Query("security_code")
+	return self, nil
+}
+
+func toBool(string string) bool {
+	return string == "true"
 }
--- service/customer_test.go
+++ service/customer_test.go
@@ -3,9 +3,34 @@ package service
 import (
 	"testing"
 
+	"time"
+
+	"fmt"
+
 	"github.com/hal-iosk/hal-cinema/model"
 )
 
 func TestCustomerImpl_Create(t *testing.T) {
-	Customer.Create(model.Customer{})
+	time := time.Now()
+	Customer.Create(model.Customer{
+		Email:            "llxo2_5oxll@icloud.com2",
+		Password:         "hoge",
+		FirstName:        "葛巻",
+		LastName:         "大樹",
+		FirstNameRead:    "かつらまき",
+		LastNameRead:     "たいき",
+		Phone:            "09096220908",
+		Address:          "高槻",
+		Birthdate:        &time,
+		Magazine:         true,
+		PointCount:       0,
+		CreditCardLimit:  "20/15",
+		CreditCardNumber: "4242 4242 4242 4242",
+		SecurityCode:     "564",
+	})
+}
+
+func TestCustomerImpl_Logincheck(t *testing.T) {
+	ok := Customer.Logincheck("llxo2_5oxll@icloud.com", "hoge")
+	fmt.Println(ok)
 }
