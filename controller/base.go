7fcde61819551e3099f22730b08d61738e12a397
makki0205 <llxo2_5oxll@icloud.com>
2017-12-07 03:16:45 +0900

    .

--- controller/base.go
+++ controller/base.go
@@ -9,7 +9,7 @@ import (
 )
 
 var DayFormat = "2006/01/02"
-var TimeFormat = "2006-01-02 15:04:05"
+var TimeFormat = "2006/01/02 15:04:05"
 
 func Batequest(msg string, c *gin.Context) {
 	c.JSON(http.StatusBadRequest, gin.H{
