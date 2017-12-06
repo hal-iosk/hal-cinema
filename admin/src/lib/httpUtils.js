e63fe8fb00b3622be712cec94691d326dc89cc99
konojunya <junya03dance@gmail.com>
2017-12-06 19:55:20 +0900

    updated

--- admin/src/lib/httpUtils.js
+++ admin/src/lib/httpUtils.js
@@ -3,10 +3,12 @@ import axios from 'axios'
 class HttpUtils {
 
   Login(email, password) {
-    return axios.post("/api/admin/signin", {
-      email,
-      password
-    })
+
+    var params = new URLSearchParams();
+    params.append('email', email);
+    params.append('password', password);
+
+    return axios.post("/api/admin/signin", params)
   }
 
 }
