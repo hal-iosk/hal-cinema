38050255c68fa0dc647dc842d31f9879b3a34250
makki0205 <llxo2_5oxll@icloud.com>
2017-12-06 19:38:10 +0900

    .

--- router/api.go
+++ router/api.go
@@ -7,12 +7,20 @@ import (
 )
 
 func apiRouter(api *gin.RouterGroup) {
-	api.POST("/signup", controller.CreateUser)
-	api.POST("/signin", session.UserLoginHandle)
 	authApi := api.Group("")
 	authApi.Use(session.AuthApiMiddleware)
+	admin := api.Group("/admin")
+	authAdmin := admin.Group("")
+	authAdmin.Use(session.AuthApiMiddleware)
+
+	api.POST("/signup", controller.CreateUser)
+	api.POST("/signin", session.UserLoginHandle)
+
 	authApi.POST("/foo", func(c *gin.Context) {
 		userID, _ := c.Get("userID")
 		c.JSON(200, userID)
 	})
+
+	admin.POST("/signin", session.AdminLoginHandle)
+
 }
