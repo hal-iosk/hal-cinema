b2366bb7a99810996d58ae3c935373ef61cf95c6
ishidakazumaDTNF <ta.kazuma12@gmail.com>
2017-12-07 12:35:37 +0900

    バグ直し中

--- public/js/header.js
+++ public/js/header.js
@@ -7,11 +7,11 @@ var isLogin = AuthManager.isLogin();
 
 if(isLogin){
 	var navLogin = document.getElementById("navLogin");
-	navLogin.innerHTML = "<a href='/view/mypage'><img src='/image/mypage.png' width='auto' height='auto'><span>マイページ</span></a>";
+	navLogin.innerHTML = "<a href='/mypage'><img src='/image/mypage.png' width='auto' height='auto'><span>マイページ</span></a>";
 
 	var navPaper = document.getElementById("navPaper");
-	navPaper.innerHTML = "<a href='/view/mypage'><img src='/image/paper.png' width='auto' height='auto'><span>予約履歴</span></a>";
+	navPaper.innerHTML = "<a href='/mypage'><img src='/image/paper.png' width='auto' height='auto'><span>予約履歴</span></a>";
 
 	var navP = document.getElementById("navP");
-	navP.innerHTML = "<a href='/view/mypage'><img src='/image/p.png' width='auto' height='auto'><br><span>チェックイン</span></a>";
+	navP.innerHTML = "<a href='/mypage'><img src='/image/p.png' width='auto' height='auto'><br><span>チェックイン</span></a>";
 }
