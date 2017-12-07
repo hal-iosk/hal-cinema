c7804a03761e3eb390cf0aea68eb7c131c0ada69
gassun <halohs50063@gmail.com>
2017-12-07 10:35:52 +0900

    .

--- public/js/watchFilm.js
+++ public/js/watchFilm.js
@@ -3,18 +3,25 @@ window.onload = function () {
   var date = new Date();
   console.log(month.getMonth() + 1);
   console.log(date.getDate());
-
+  
   var scheduleTable = document.getElementById("scheduleTable");
 
   for (var scheduleWeek = 0; scheduleWeek <= 6; scheduleWeek++) {
     var scheduleDate = document.createElement("li");
     scheduleDate.classList.add("scheduleDate");
-    if(scheduleWeek<=2){
-        scheduleDate.classList.add("three");
-    }else{
-        scheduleDate.classList.add("other"); 
-    }
+    scheduleDate.classList.add((month.getMonth() + 1) + "/" + (date.getDate() + scheduleWeek));
     scheduleDate.innerHTML = (month.getMonth() + 1) + "/" + (date.getDate() + scheduleWeek);
+    
     scheduleTable.appendChild(scheduleDate);
+    var day = document.getElementsByClassName((month.getMonth() + 1) + "/" + (date.getDate() + scheduleWeek).innerHTML);
+    scheduleDate.onclick = function(){ 
+        paging(day);
+        };
   }
+
+}
+var de = document.getElementsByClassName("scheduleDate").innerHTML;
+console.log(de);
+function paging(day){
+ alert(day);
 }
