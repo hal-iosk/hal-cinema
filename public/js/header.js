d0ef43853d2c911f3102940168f137dfa57a9b70
ishidakazumaDTNF <ta.kazuma12@gmail.com>
2017-12-07 10:12:21 +0900

    俺のものやぞ、まさたか？

---
+++ public/js/header.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(".drawer").drawer();
+});
+
+//ログイン制御
+var isLogin = AuthManager.isLogin();
+
+if(isLogin){
+	var navLogin = document.getElementById("navLogin");
+	navLogin.innerHTML = "<a href='/view/mypage.html'><img src='/image/mypage.png' width='auto' height='auto'><span>マイページ</span></a>";
+
+	var navPaper = document.getElementById("navPaper");
+	navPaper.innerHTML = "<a href='/view/mypage.html'><img src='/image/paper.png' width='auto' height='auto'><span>予約履歴</span></a>";
+
+	var navP = document.getElementById("navP");
+	navP.innerHTML = "<a href='/view/mypage.html'><img src='/image/p.png' width='auto' height='auto'><br><span>チェックイン</span></a>";
+}
