9efbb68aff65ef9351c2ccb76123925e7969c3cd
konojunya <junya03dance@gmail.com>
2017-12-09 17:05:06 +0900

    updated

--- client/src/components/reserve/nav.vue
+++ client/src/components/reserve/nav.vue
@@ -1,8 +1,10 @@
 <template>
   <ul>
-    <li>座席選択</li>
-    <li>チケット選択</li>
-    <li>購入内容の確認</li>
+    <li class="navigation-item isActive">座席選択</li>
+    <li>></li>
+    <li class="navigation-item">チケット選択</li>
+    <li>></li>
+    <li class="navigation-item">購入内容の確認</li>
   </ul>
 </template>
 
@@ -14,8 +16,17 @@ export default {
 
 <style lang="scss" scoped>
 ul {
-  width: 80%;
-  margin: 0 auto;
   display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 30px 20%;
+  box-shadow: 0px 0px 1px black;
+  li.navigation-item {
+    letter-spacing: 1.5px;
+  }
+  li.isActive { font-weight: bold; color: #B71C1C; box-shadow: 0px 0px 1px #B71C1C; }
+  li {
+    padding: 10px;
+  }
 }
 </style>
--- client/src/components/reserve/ticketType.vue
+++ client/src/components/reserve/ticketType.vue
@@ -3,69 +3,112 @@
 
     <reserve-nav></reserve-nav>
 
-    <h1>チケット券種選択</h1>
+    <movie-content
+      :title="movieContent.title"
+      :date="movieContent.date"
+      :theater="movieContent.theater"
+    ></movie-content>
 
-    <ul>
-      <li>
-        <p>作品名</p>
-        <p>あさひなぐ</p>
-      </li>
-      <li>
-        <p>日時</p>
-        <p>2017/09/26 16:20 ~ 18:50</p>
-      </li>
-      <li>
-        <p>シアター番号</p>
-        <p>1</p>
-      </li>
-    </ul>
-
-    <ul>
-      <li>
+    <ul class="container">
+      <li v-for="seat in seats">
         <div class="img">
           <img src="/image/seat.png" alt="シートイラスト">
         </div>
-        <p>N1</p>
+        <p>{{seat.id}}</p>
         <b-select
             placeholder="券種を選んでください。"
             size="is-medium"
             expanded
             required
+            @select="selected"
         >
-          <option value="flint">Flint</option>
-          <option value="silver">Silver</option>
+          <option value="1">Flint</option>
         </b-select>
       </li>
     </ul>
 
-    <div>
+    <div class="controller">
       <button class="button" @click="back">戻る</button>
       <button class="button is-primary" @click="next">次へ</button>
     </div>
+
   </section>
 </template>
 
 <script>
 import reservePayload from '../../lib/reserve.class'
 import ReserveNav from './nav.vue'
+import MovieContent from './movieContent.vue'
 
 export default {
   name: "ticketType",
   mounted() {
     if(reservePayload.getRequestData().length <= 0) this.$router.push({ path: "/reserve" });
   },
+  data() {
+    return {
+      movieContent: {
+        title: "あさひなぐ",
+        date: "2017/09/26 10:00 〜 11:00",
+        theater: "1"
+      },
+      seats: reservePayload.getSeats().map((seat) => {
+        return {
+          id: seat,
+          ticketType: null
+        }
+      })
+    }
+  },
   methods: {
+    selected() {
+      console.log("hoge")
+    },
     back() {
-      this.$router.push({ path: "/reserve" })
       reservePayload.clear()
+      location.href = "/reserve"
     },
     next() {
-      reservePayload.setPriceId(1)
-      this.$router.push({ path: "/reserve/complete" })
+      console.log(this.selectTicket)
+      // reservePayload.setPriceId(1)
+      // this.$router.push({ path: "/reserve/complete" })
     }
   },
   components: {
-    ReserveNav
+    ReserveNav,
+    MovieContent
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.controller {
+  width: 50%;
+  margin: 0 auto 50px;
+  display: flex;
+  justify-content: space-around;
+}
+.container {
+  width: 80%;
+  margin: 20px auto;
+  padding: 30px 0;
+  li {
+    margin: 30px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .img {
+      width: 50px;
+      img {
+        width: 100%;
+        display: block;
+      }
+    }
+    p {
+      font-size: 1.5rem;
+      margin-right: auto;
+      margin-left: 50px;
+    }
+  }
+}
+</style>
