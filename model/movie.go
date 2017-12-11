58f304086a9a2f61d07b9c1205a8c8bd98d7e530
makki0205 <llxo2_5oxll@icloud.com>
2017-12-11 17:49:11 +0900

    .

--- model/movie.go
+++ model/movie.go
@@ -2,6 +2,6 @@ package model
 
 func (m Movie) GetSchedule() []ScreeningSchedule {
 	var Schedules []ScreeningSchedule
-	db.Where("movie_id = ?", m.ID).Find(&Schedules)
+	db.Where("movie_id = ? AND `release` = ?", m.ID, true).Find(&Schedules)
 	return Schedules
 }
