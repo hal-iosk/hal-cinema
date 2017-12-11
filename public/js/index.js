5a8ca3b74b6e88fa1a4730de32fb11e7eab95ca5
ishidakazumaDTNF <ta.kazuma12@gmail.com>
2017-12-11 11:06:51 +0900

    up

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ $(function(){
 	// #で始まるリンクをクリックしたら実行されます
 	$('a[href^="#"]').click(function() {
 	  // スクロールの速度
-	  var speed = 400; // ミリ秒で記述
+	  var speed = 250; // ミリ秒で記述
 	  var href= $(this).attr("href");
 	  var target = $(href == "#" || href == "" ? 'html' : href);
 	  var position = target.offset().top;
